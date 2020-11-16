15ed3a6d17725925d81ad5fb66457032a1f0c553
ludwigcarlsson <zhendiac@gmail.com>
2020-11-16 17:08:10 +0100

    feat: added design and routing for trainer, pokemon card and almost catalogue

--- src/app/components/trainer/trainer.component.ts
+++ src/app/components/trainer/trainer.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainerComponent implements OnInit {
 
+  public pokemoncards: any = 
+  [{ 
+    title: 'ditto',
+    type: 'normal',
+    id: 1
+  }, {
+    title: 'eevee',
+    type: 'normal',
+    id: 2
+  }, {
+    title: 'pidgey',
+    type: 'normal',
+    id: 3
+  }]
+
   constructor() { }
 
   ngOnInit(): void {
+    const container = document.getElementById('login-container');
+    if (localStorage.getItem('username') != '' && container != null) {
+      container.remove();
+    }
   }
 
 }
