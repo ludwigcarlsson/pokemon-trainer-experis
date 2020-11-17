30d9031c1034a2457d553f53ec21b7587c1fa707
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2020-11-17 16:59:50 +0100

    LudwigCarlsson & Vattenkruka feat: Added api fetch, listing of pokemon names on catalogue page and styling.

--- src/app/components/catalogue/catalogue.component.ts
+++ src/app/components/catalogue/catalogue.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-catalogue',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatalogueComponent implements OnInit {
 
-  constructor() { }
+  public pokemons: any = [];
+
+
+  constructor(private api: ApiService) { }
+
+  async displayCatalogue() {
+    try {
+      const data = await this.api.fetchAllPokemons();
+
+      for (let i = 0; i < data.results.length; i++) {
+        this.displayThisPokemon(data.results[i].url);
+      }
+  
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async displayThisPokemon(URL: any) {
+    try {
+      const data = await this.api.fetchThisPokemon(URL)
+
+      console.log(data);
+      
+
+      this.pokemons.push({
+        name: data.name,
+        height: data.height,
+        weight: data.weight,
+        abilities: data.abilities,
+        sprite: data.sprites.front_default,
+        img: data.sprites.other, // weird format, fråga Sean
+        moves: data.moves,
+        type: data.types,
+        baseExp: data.base_experience,
+        stats: data.stats
+      });
+
+    } catch (e) {
+      console.log(e);
+      
+    }
+  }
 
   ngOnInit(): void {
+    this.displayCatalogue();
   }
 
 }
--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private BASE_URL: string = 'https://pokeapi.co/api/v2/pokemon';
+  private limitOfPokemons : string = '10';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  fetchAllPokemons(): Promise<any> {
+    return this.http
+      .get(this.BASE_URL +'?limit=' + this.limitOfPokemons)
+      .toPromise();
+  }
+
+  fetchThisPokemon(URL: any): Promise<any> {
+    return this.http
+      .get(URL)
+      .toPromise();
+  }
 }
