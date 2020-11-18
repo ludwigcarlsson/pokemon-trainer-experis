436c6ae37aa6955a7ed4d393d1cdc6eaf5b4c649
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2020-11-18 17:10:08 +0100

    Vattenkruka & LudwigCarlsson feat: visualising data to the pokemon card on pokemon page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TrainerComponent } from './components/trainer/trainer.component';
 import { PokemonComponent } from './components/pokemon/pokemon.component';
 import { CatalogueComponent } from './components/catalogue/catalogue.component';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { CatalogueComponent } from './components/catalogue/catalogue.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
