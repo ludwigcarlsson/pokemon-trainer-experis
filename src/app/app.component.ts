ed90e007302d319a13e03fd1a802c38c7e2e93e4
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2020-11-22 16:13:17 +0100

    Vattenkruka & LudwigCarlsson - Feat : Added a toogle music button and an audio file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   
+  public audio: any = new Audio();
+  public playing: boolean = false;
+  public started: boolean = false;
   constructor () {}
 
   ngOnInit(): void {
   }
   
+  playAudio(){
+    this.audio.src = "../assets/audio/pokemon-song.mp3"
+    this.audio.volume = 0.05;
+    this.audio.load();
+    this.audio.loop = true;
+    this.audio.play();
+  }
+
+  toggleAudio() {
+    if(!this.started){
+      this.playAudio();
+      this.started = true;
+      this.playing = true;
+    } else {
+      if (this.playing){
+        this.playing = false;
+        this.audio.volume = 0;
+      } else {
+        this.playing = true;
+        this.audio.volume = 0.05;
+      }
+    }
+  }
 }
